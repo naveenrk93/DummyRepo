95fefd413f33de58381ff58125c11d37305cd078
Naveenramkumar Varadarajan <nvaradarajan@paypal.com>
2018-02-13 15:30:07 -0800

    Test 18

--- configure_aws.sh
+++ configure_aws.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOL
 [default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+aws_access_key_id = ${AK}
+aws_secret_access_key = ${SK}
 region = us-west-2
 EOL
