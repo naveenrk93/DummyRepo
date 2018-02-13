ec2c0ed48e958c5e59450156c3d3291828ed0458
Naveenramkumar Varadarajan <nvaradarajan@paypal.com>
2018-02-13 14:49:47 -0800

    Test 14

--- configure_aws.sh
+++ configure_aws.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
 sudo mkdir -p ~/.aws
-
-sudo cat > ~/.aws/credentials <<
-[default]
-aws_access_key_id = ${AK}
-aws_secret_access_key = ${SK}
-region = us-west-2
-output = json
