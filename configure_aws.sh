0839c09dbda0ebcc63773ea9fa067a19f10c832a
Naveenramkumar Varadarajan <nvaradarajan@paypal.com>
2018-02-13 14:33:26 -0800

    Test 12

--- configure_aws.sh
+++ configure_aws.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.aws
+sudo kdir -p ~/.aws
 
-cat > ~/.aws/credentials << EOL
+sudo cat > ~/.aws/credentials << EOL
 [default]
 aws_access_key_id = ${AK}
 aws_secret_access_key = ${SK}
