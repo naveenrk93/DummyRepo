bf5703db6c3ceb7532b96f2480e3aab1c125c038
Naveenramkumar Varadarajan <nvaradarajan@paypal.com>
2018-02-13 14:45:37 -0800

    Test 13

--- configure_aws.sh
+++ configure_aws.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-sudo kdir -p ~/.aws
+sudo mkdir -p ~/.aws
 
-sudo cat > ~/.aws/credentials << EOL
+sudo cat > ~/.aws/credentials <<
 [default]
 aws_access_key_id = ${AK}
 aws_secret_access_key = ${SK}
-region=us-west-2
-EOL
+region = us-west-2
+output = json
